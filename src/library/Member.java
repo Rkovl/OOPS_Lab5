e953b5f09e0b6be9e4785beab08a8fcb5aa7e385
Ralph Juliano <ralph.juliano@myokanagan.bc.ca>
2025-10-28 13:18:43 -0700

    Update Member.java

--- src/library/Member.java
+++ src/library/Member.java
@@ -6,8 +6,23 @@ public class Member {
 	// Can borrow and return books.
 
 	private String name;
-	// private borrowedbBooks TODO: implement collection of borrowed books
-	
-	// TODO: implement functionality of Member class
+	private String memberID;
+	private List<Book> borrowedBooks;
+
+	public Member(String memberID, String name) {
+		this.memberID = memberID;
+		this.name = name;
+		this.borrowedBooks = new ArrayList<>();
+	}
+
+	public void borrowBook(Book book) {
+		if (book.isAvailable()) {
+			book.borrowBook();
+			borrowedBooks.add(book);
+			System.out.println(name + " borrowed " + book.getTitle());
+		} else {
+			System.out.println(book.getTitle() + " is not available for borrowing.");
+		}
+	}
 
 }
