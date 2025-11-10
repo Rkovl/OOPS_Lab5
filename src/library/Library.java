3ce3e56e41b8df0d6c20902512272c9bb9d4a70e
Ryan Kovlaske <108691921+Rkovl@users.noreply.github.com>
2025-11-10 11:48:27 -0800

    Update Library.java

--- src/library/Library.java
+++ src/library/Library.java
@@ -1,5 +1,8 @@
 package library;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Library {
 	
 	//  GRASP Principles: Creator, Controller
@@ -11,6 +14,41 @@ public class Library {
 	// private catalog (list of Book) 
 	// private members (list of Members)
 
-	// TODO: implement functionality of Member class
+    private HashMap<String, Book> catalog;
+    private HashMap<String, Member> members;
 
+	// DONE: implement functionality of Member class
+    public Library() {
+        catalog = new HashMap<>();
+        members = new HashMap<>();
+    }
+
+    public void addBook(String title, String author) {
+        catalog.put(title, new Book(title, author));
+    }
+    public void removeBook(String title) {
+        catalog.remove(title);
+    }
+
+    public void addMember(String name){
+        members.put(name, new Member(name));
+    }
+    public void removeMember(String name){
+        members.remove(name);
+    }
+
+    public void showAvailableBooks() {
+        for (Book book : catalog.values()) {
+            if (book.isAvailable()) {
+                System.out.println(book.toString());
+            }
+        }
+    }
+    public void showUnavailableBooks() {
+        for (Book book : catalog.values()) {
+            if (!book.isAvailable()) {
+                System.out.println(book.toString());
+            }
+        }
+    }
 }
