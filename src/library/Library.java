3d389110d3a20101b8eb5b5aa73451a62c8904e3
Maddison <voidwalker604@protonmail.com>
2025-11-10 11:55:55 -0800

    library Class created

--- src/library/Library.java
+++ src/library/Library.java
@@ -1,16 +1,58 @@
 package library;
 
+import java.util.ArrayList;
+
 public class Library {
-	
-	//  GRASP Principles: Creator, Controller
-	//	Manage the catalog of books and members.
-	//	Track which books are borrowed and available
-	
-	
-	// Attributes:
-	// private catalog (list of Book) 
-	// private members (list of Members)
 
-	// TODO: implement functionality of Member class
+    //  GRASP Principles: Creator, Controller
+    //	Manage the catalog of books and members.
+    //	Track which books are borrowed and available
 
+
+    // Attributes:
+    // private catalog (list of Book)
+    // private members (list of Members)
+
+    // DONE: implement functionality of Member class
+    private ArrayList<Book> catalog = new ArrayList<>();
+    private ArrayList<Member> members = new ArrayList<>();
+
+    public Library() {
+    }
+
+    public void addBook(Book book) {
+        catalog.add(book);
+    }
+
+    public void removeBook(Book book) {
+        catalog.remove(book);
+    }
+
+    public void addMember(Member member) {
+        members.add(member);
+    }
+
+    public void removeMember(Member member) {
+        members.remove(member);
+    }
+
+    public ArrayList<Book> availableBooks() {
+        ArrayList<Book> availableBooks = new ArrayList<>();
+        for (Book book : catalog)
+            if (book.isAvailable())
+                availableBooks.add(book);
+
+        return availableBooks;
+    }
+
+    public ArrayList<Book> borrowedBooks() {
+
+        ArrayList<Book> borrowedBooks = new ArrayList<>();
+        for (Book book : catalog)
+            if (!book.isAvailable())
+                borrowedBooks.add(book);
+
+        return borrowedBooks;
+
+    }
 }
